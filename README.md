395956d8e8ab6bf45f870030048ffe26196a3fef
Parth Phadke <parthphadke2004@gmail.com>
2026-06-02 12:39:29 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Medical-Chatbot
+
+
+git clone https://github.com/parthphadke2004-create/Medical-Chatbot.git
+
+
+conda create -n medibot python=3.10 -y
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Parth Phadke",
+    author_email="parthphadke2004@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
